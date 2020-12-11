6f72329a0a2185558f8c686a092b860ec4f139c2
julian <juliancano.dev@gmail.com>
2020-12-10 19:34:01 -0500

    Trying

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
 
+def checkKeys(key, boxes, open_boxes):
+    """This is the function to use recursion."""
+    for element in boxes[key]:
+    
+        if (element < len(boxes) and open_boxes[element] == False and boxes[key] != []):
+            open_boxes[element] = True
+            open_boxes = checkKeys(element, boxes, open_boxes)
+        else:
+            break
 
-def join(T, R):
-    res = []
-    for e in R:
-        res += T[e]
-    return res
+    return open_boxes
 
 
 def canUnlockAll(boxes):
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
+    """This function returns True if all the boxes can be unlocked and False if they cannon."""
+    lis_en = len(boxes)
 
-    return len(total) == len(boxes)
+    if boxes == None or lis_en == 0:
+        return False
+
+    open_boxes = [False] * lis_en
+    open_boxes[0] = True
+
+    for key in boxes[0]:
+        if (key < lis_en and open_boxes[key] == False):
+            open_boxes[key] = True
+            open_boxes = checkKeys(key, boxes, open_boxes)
+        else:
+            continue
+
+    if (False in open_boxes):
+        return (False)
+    return (True)
