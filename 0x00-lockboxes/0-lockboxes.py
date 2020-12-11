74024b972609ca589726bd58b1edb8376cbe1da4
julian <juliancano.dev@gmail.com>
2020-12-10 19:45:01 -0500

    Trying

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
-
-def checkKeys(key, boxes, open_boxes):
-    """This is the function to use recursion."""
-    for element in boxes[key]:
-    
-        if (element < len(boxes) and open_boxes[element] == False and boxes[key] != []):
-            open_boxes[element] = True
-            open_boxes = checkKeys(element, boxes, open_boxes)
-        else:
-            break
-
-    return open_boxes
+"""
+Determines if all the boxes can be opened.
+"""
 
 
 def canUnlockAll(boxes):
-    """This function returns True if all the boxes can be unlocked and False if they cannon."""
-    lis_en = len(boxes)
+    """Determines if all the boxes can be opened"""
+    # number of boxes
+    n = len(boxes)
+    # found keys
+    keys = [0]
 
-    if boxes == None or lis_en == 0:
+    # looping through boxes
+    for key in keys:
+        box = boxes[key]
+        # looping inside a box
+        for new_key in box:
+            if new_key not in keys and new_key < n:
+                # Add new key
+                keys.append(new_key)
+    # Are number of boxes and keys equal
+    if n == len(keys):
+        return True
+    else:
         return False
-
-    open_boxes = [False] * lis_en
-    open_boxes[0] = True
-
-    for key in boxes[0]:
-        if (key < lis_en and open_boxes[key] == False):
-            open_boxes[key] = True
-            open_boxes = checkKeys(key, boxes, open_boxes)
-        else:
-            continue
-
-    if (False in open_boxes):
-        return (False)
-    return (True)
