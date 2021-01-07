b2713ad805124ffe831e8c63e154ddbd02c9492a
julian <juliancano.dev@gmail.com>
2021-01-07 10:54:42 -0500

    Task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Main program.
+"""
+
+
+def minOperations(n):
+    	"""
+	Program minOperation
+    	"""
+
+	if type(n) is not int or n <= 0:
+        return 0
+    	operations = 0
+    	H = 1
+    	copy_all = 0
+    	paste = 0
+    	H_copied = 0
+    	while H < n:
+        	if n % H == 0:
+            	copy_all += 1
+            	H_copied = H
+        	paste += 1
+        	operations = copy_all + paste
+        	H += H_copied
+    	return operations
