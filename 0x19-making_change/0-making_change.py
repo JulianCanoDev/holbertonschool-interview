3ff36df3f3b90d86d4913dd4de5c84444c403959
Julian Cano VR <juliancano.dev@gmail.com>
2021-06-22 15:24:54 -0500

    Change 0

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,29 +4,21 @@ Given a pile of coins of different values, determine the fewest number of coins.
 """
 
 
-def _change_matrix(coin_set, change_amount):
-    """Function to create the matrix we'll use for the optimization."""
-    matrix = [[0 for m in range(change_amount + 1)] for m in range(len(coin_set) + 1)]
-    for i in range(change_amount + 1):
-        matrix[0][i] = i
-    return matrix
-
 def makeChange(coins, total):
     """Given a pile of coins of different values."""
     if total == 0:
         return 0
 
-    matrix = _change_matrix(coins, total)
-    for c in range(1, len(coins) + 1):
-        for r in range(1, total + 1):
+    value = len(coins) - 1
+    num_coins = 0
+    coins.sort()
 
-            if coins[c-1] == r:
-                matrix[c][r] = 1
+    while total > 0:
+        while coins[value] > total and value >= 0:
+            value -= 1
+        total -= coins[value]
+        num_coins += 1
 
-            elif coins[c-1] > r:
-                matrix[c][r] = matrix[c-1][r]
-
-            else:
-                matrix[c][r] = min(matrix[c - 1][r], 1 + matrix[c][r - coins[c - 1]])
-
-    return matrix[-1][-1]
+    if total < 0:
+        return -1
+    return num_coins
