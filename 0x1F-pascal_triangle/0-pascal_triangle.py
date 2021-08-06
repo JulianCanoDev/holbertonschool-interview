f176c7a71d956bd0efb3efca667bbe45244ee7b1
Julian Cano VR <juliancano.dev@gmail.com>
2021-08-05 20:10:24 -0500

    task 0

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,16 @@
 0-pascal_triangle
 """
 
-def pascal_triangle(n):
-	"""pascal"""
-	pascal = []
 
-	for i in range(0, n):
-		pascal.append([])
-		for j in range(0, i + 1):
-			if j == 0 or j == i:
-				pascal[i].append(1)
-			else:
-				pascal[i].append(pascal[i - 1][j] + pascal[i - 1][j - 1])
-	return pascal
+def pascal_triangle(n):
+    """pascal"""
+    pascal = []
+
+    for i in range(0, n):
+        pascal.append([])
+        for j in range(0, i + 1):
+            if j == 0 or j == i:
+                pascal[i].append(1)
+            else:
+                pascal[i].append(pascal[i - 1][j] + pascal[i - 1][j - 1])
+    return pascal
